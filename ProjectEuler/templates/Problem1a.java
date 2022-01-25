4770f46b29318507d4e93608590c17b08c803d57
Sophie Engle <sjengle@cs.usfca.edu>
2022-01-24 21:39:06 -0800

    Adding templates

---
+++ ProjectEuler/templates/Problem1a.java
@@ -0,0 +1,7 @@
+package edu.usfca.cs272;
+
+public class Problem1a {
+	public static void main(String[] args) {
+		// TODO Fill in solution
+	}
+}
---
+++ ProjectEuler/templates/Problem1c.java
@@ -0,0 +1,13 @@
+package edu.usfca.cs272;
+
+public class Problem1c {
+	public static int inputNonNegativeInteger() {
+		// TODO Fill in solution
+		throw new UnsupportedOperationException("Not yet implemented.");
+	}
+
+	public static void main(String[] args) {
+		// TODO Fill in solution
+		throw new UnsupportedOperationException("Not yet implemented.");
+	}
+}
