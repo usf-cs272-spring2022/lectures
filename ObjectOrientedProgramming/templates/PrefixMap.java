406abc64fefe778b4ac6caa723bfc6ba4338b535
Sophie Engle <sjengle@cs.usfca.edu>
2022-02-15 12:04:40 -0800

    Adding prefix map demos

---
+++ ObjectOrientedProgramming/templates/PrefixMap.java
@@ -0,0 +1,22 @@
+package edu.usfca.cs272;
+
+public class PrefixMap {
+	// TODO Create members
+
+	public PrefixMap(int prefixSize) {
+		// TODO Fill in
+		throw new UnsupportedOperationException("Not yet implemented: " + prefixSize);
+	}
+
+	public PrefixMap() {
+		// TODO Fill in
+		throw new UnsupportedOperationException("Not yet implemented.");
+	}
+
+	public boolean addWord(String word) {
+		// TODO Fill in
+		throw new UnsupportedOperationException("Not yet implemented: " + word);
+	}
+
+	// TODO Add other methods
+}
